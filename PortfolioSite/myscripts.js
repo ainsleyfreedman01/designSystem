2a4f567f73a12d875fada99f2d099c8ff4667ccf
ainsleyfreedman01 <ainsleyfreedman01@gmail.com>
2024-05-04 21:47:05 -0400

    commit

--- PortfolioSite/myscripts.js
+++ PortfolioSite/myscripts.js
@@ -1,3 +1,5 @@
 const navigationHeight = document.querySelector('.navigation').offsetHeight;
 
-console.log(navigationHeight);
+//console.log(document.documentElement);
+
+document.documentElement.style.setProperty('--scroll-padding', navigationHeight + "px");
